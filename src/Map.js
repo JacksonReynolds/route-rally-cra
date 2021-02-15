bdfef33c8dd4f8e3abd47b0538a620e77d5d6cf5
Jackson <jwreynold@gmail.com>
2021-02-14 23:38:39 -0500

    building extra simple map

--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
 
-const Map = () => {
-  return (
-    <div>
-      Map goes here
-    </div>
-  )
+export default class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 39.968,
+      long: -98.3
+    },
+    zoom: 5
+  }
+
+  render() {
+    return (
+      <div class="Map">
+        <GoogleMapReact
+
+        >
+
+        </GoogleMapReact>
+      </div>
+    )
+  }
 }
 
-export default Map
